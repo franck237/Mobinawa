98bb1720cdd3c7ccfb53a595cd43c2f07efcf506
franck237 <franck.kamanou@gmail.com>
2019-11-25 13:38:20 +0100

    Creating a controller for countries

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'countries/new'
+  get 'countries/create'
+  get 'countries/update'
+  get 'countries/edit'
+  get 'countries/destroy'
+  get 'countries/index'
+  get 'countries/show'
   devise_for :admins
   root to: 'sector#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
