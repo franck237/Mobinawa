fe585c7526bf88bce856de94b9263b532226b010
franck237 <franck.kamanou@gmail.com>
2019-11-25 11:24:04 +0100

    Adding the Sector model

---
+++ db/migrate/20191125102342_create_sectors.rb
@@ -0,0 +1,10 @@
+class CreateSectors < ActiveRecord::Migration[6.0]
+  def change
+    create_table :sectors do |t|
+      t.string :name
+      t.string :icon
+
+      t.timestamps
+    end
+  end
+end
