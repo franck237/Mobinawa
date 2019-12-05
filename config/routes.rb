b72ca28548b935500bf1b7b4aed84cf22be24a2c
franck237 <franck.kamanou@gmail.com>
2019-12-05 16:05:36 +0100

    Modification of the view Sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :admins, controllers: { sessions: 'admins/sessions' }
-  devise_for :admins, controllers: { registrations: 'admins/registrations' }
   root to: 'countries#show'
   
   resources :admins do
