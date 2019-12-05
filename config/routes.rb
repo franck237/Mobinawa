cbaedcd980472f39ec42ddb47686a5e06a2a02a7
franck237 <franck.kamanou@gmail.com>
2019-12-05 14:00:39 +0100

    Configuration of devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admins, controllers: { sessions: 'admins/sessions' }
+  devise_for :admins, controllers: { registrations: 'admins/registrations' }
   root to: 'countries#show'
   
   resources :admins do
