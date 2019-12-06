cac5f934e8ccb20ef415708aa3bb4c0e7e852a42
franck237 <franck.kamanou@gmail.com>
2019-12-06 23:47:16 +0100

    Adding Background css and Starting front end for sectors#index

--- app/controllers/sectors_controller.rb
+++ app/controllers/sectors_controller.rb
@@ -1,14 +1,24 @@
 class SectorsController < ApplicationController
   def index
+    @sectors = Sector.all 
   end
 
   def show
+    @sector = Sector.find(params[:id])
+
   end
 
   def new
+    @sector = Sector.new
   end
 
   def create
+    @sector = Sector.new(name: params[:name], icon: params[:icon])
+      if @sector.save
+        redirect_to sector_path(@sector.id)
+      else
+        render 'new'
+      end
   end
 
   def edit
@@ -18,5 +28,6 @@ class SectorsController < ApplicationController
   end
 
   def destroy
+    @sector = Sector.destroy
   end
 end
