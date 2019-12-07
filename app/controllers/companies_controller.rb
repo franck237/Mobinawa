c25348c11b95bb61b9fd5875477cba2ea40ee315
franck237 <franck.kamanou@gmail.com>
2019-12-07 14:27:39 +0100

    Starting front end for companies#show

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,14 +1,23 @@
 class CompaniesController < ApplicationController
   def index
+    @companies = Company.all 
   end
 
   def show
+    @company = Company.find(params[:id])
   end
 
   def new
+    @company = Company.new 
   end
 
   def create
+    @company = Company.new(number: params[:number], name: params[:name], email: params[:email], website: params[:website], logo: params[:logo], description: params[:description], sub_sector_id: params[:sub_sector_id], admin_id: params[:admin_id], country_id: params[:country_id])
+      if @company.save
+        redirect_to Company_path(@Company.id)
+      else
+        render 'new'
+      end
   end
 
   def edit
@@ -18,5 +27,6 @@ class CompaniesController < ApplicationController
   end
 
   def destroy
+    @Company = Company.destroy
   end
 end
