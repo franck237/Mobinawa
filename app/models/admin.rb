9e0195d49e403c55f06e13dcb89faac6323d3385
franck237 <franck.kamanou@gmail.com>
2019-12-09 16:11:39 +0100

    Adding companies & products associations into the admin model, with modification of the view admin#show to include companies

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -10,12 +10,14 @@ class Admin < ApplicationRecord
   validates :function, presence: true
 
   belongs_to :country
+  has_many :companies
+  has_many :products, through: :companies
 
   #Useful in order to send an email to an admin after creating an account
   after_create :welcome_send
 
   def fullname
-  	firstname + " " + lastname
+  	firstname.capitalize + " " + lastname.upcase
   end
 
   def welcome_send
