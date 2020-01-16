71415cd26cb0efba7946b0a3f5ed3260cd422eb0
franck237 <franck.kamanou@gmail.com>
2020-01-16 15:13:28 +0100

    Migration of AddTypeToProducts in order to select article or service for a product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_11_091631) do
+ActiveRecord::Schema.define(version: 2020_01_16_140837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(version: 2019_12_11_091631) do
     t.bigint "company_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "type"
     t.index ["company_id"], name: "index_products_on_company_id"
   end
 
