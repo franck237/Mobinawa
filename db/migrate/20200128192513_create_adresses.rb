3da23392b54a0551a58c2e7c6bb13945f101870b
franck237 <franck.kamanou@gmail.com>
2020-01-28 20:43:04 +0100

    Modifying the Adress migration for specifying GPS attributes lat & lng

--- db/migrate/20200128192513_create_adresses.rb
+++ db/migrate/20200128192513_create_adresses.rb
@@ -3,8 +3,8 @@ class CreateAdresses < ActiveRecord::Migration[6.0]
     create_table :adresses do |t|
       t.string :name
       t.references :city, null: false, foreign_key: true
-      t.decimal :lat
-      t.decimal :lng
+      t.decimal :lat, precision: 10, scale: 6
+      t.decimal :lng, precision: 10, scale: 6
 
       t.timestamps
     end
